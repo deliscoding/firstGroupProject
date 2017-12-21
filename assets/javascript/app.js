673c881357450d75a071cc413956099e9dda7070
Mark Powalisz <markpowalisz@Marks-MacBook-Pro.local>
2017-12-21 13:04:53 -0500

    Moved trivia to a seperate page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+// LET'S GET WEIRD
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAeAwiIUQprC7KrQhVym-xOkQgz1iHYeUE",
@@ -22,5 +24,36 @@ $.ajax({
 
 }).done(function (response) {
     console.log(response);
+
+    var results = response.results;
+    console.log(results);
+
+    for (var i = 0; i < results.length; i++) {
+
+        // Creating a paragraph tag with the result's rating
+        
+        $("#questions").append('<p> Category: ' + results[i].category + '</p>');
+        $("#questions").append('<p> Question: ' + results[i].question + '</p>');
+        $("#questions").append('<p> A: ' + results[i].correct_answer + '</p>');
+
+        // incorrectAnswer = results[i].incorrectAnswers[x]
+        // console.log(incorrectAnswer)
+
+        // for (var x = 0; i < incorrectAnswer.length; x++) {
+
+        //     $("#questions").append('<p> B: ' + incorrectAnswer[x] + '</p>')
+            //     p.text("C: " + incorrectAnswer[x])
+            //     p.text("D: " + incorrectAnswer[x])
+        // }
+
+
+        // Setting the src attribute of the image to a property pulled off the result item
+        // p.addClass("trivia");
+
+        // Appending the paragraph and image tag to the resultsDiv
+        // $("#questions").append(p);
+    }
+
 })
+
 
