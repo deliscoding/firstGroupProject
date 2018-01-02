33e09107276c0553e205478820a4c6b94c5ecc39
Mark Powalisz <markpowalisz@Marks-MacBook-Pro.local>
2018-01-01 23:40:04 -0500

    Changes from previous merge did not seem to take.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,36 +9,6 @@
 //     storageBucket: "",
 //     messagingSenderId: "108877913352"
 // };
-// Initialize Jason's Firebase
-var config = {
-    apiKey: "AIzaSyDywo8of7_TlZKH_SlysnkRIJlyHSBUds0",
-    authDomain: "guzzle-puzzle.firebaseapp.com",
-    databaseURL: "https://guzzle-puzzle.firebaseio.com",
-    projectId: "guzzle-puzzle",
-    storageBucket: "guzzle-puzzle.appspot.com",
-    messagingSenderId: "355498500330"
-  };
-  firebase.initializeApp(config);
-  firebase.auth().signInAnonymously().catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-        console.error(error);
-    })
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user)   {    
-    var uid = user.uid;
-        console.log(uid);
-    localStorage.setItem("gpfuid", uid)
-    }});
-    
-var database = firebase.database();
-//var fuid = localStorage.getItem("gpfuid");
-var fuid = firebase.auth().currentUser.uid;
-
-
-// function logOut();
-//     firebase.auth().signOut();
-//     $(#quit).on("click", function(logOut));
 
 // Pulls Trivia Database API
 var queryURL = "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple";
@@ -80,71 +50,5 @@ $.ajax({
         // Appending the paragraph and image tag to the resultsDiv
         // $("#questions").append(p);
     }
-
-})
-// This is the submit button tied to the govna page
-$("#submit").on("click", function (event) {
-    console.log("Submit btn clicked")
-    event.preventDefault();
-    var safeWord = $("#safeWord").val().trim();
-    console.log(safeWord);
-
-    guzzPuzz = {
-        safeWord: safeWord,
-    }
-
-    // push guzzPuzz to firebase
-    database.ref().child("users/" + fuid).set(guzzPuzz);
-
-    // request snap of firebase
-    database.ref().on("value", function (snap) {
-
-        // stores safeWord as userWord
-        var userWord = snap.val().safeWord;
-        
-        var p = $("<p>");
-        p.append("You chose: " + userWord);
-        $("#userWord").append(p)
-
-    })
-
-})
-
-// This is the reSubmit button tied to the verify pageXOffset.
-$("#reSubmit").on("click", function (event) {
-    console.log("reSubmit btn clicked")
-    event.preventDefault();
-    var verify = $("#verify").val().trim();
-    console.log(verify);
-
-    guzzPuzz = {
-        safeWord: safeWord,
-        verify: verify,
-    }
-
-    // push guzzPuzz to firebase
-    database.ref().child("users/" + fuid).set(verify);
-    // database.ref().set(verify);
-
-    // request snap of firebase
-    database.ref().on("value", function (snap) {
-
-        // stores safeWord as userWord
-        var userWord = snap.val().safeWord;
-        
-       if (verify == userWord) {
-        var p = $("<p>");
-        p.append("You entered: " + verify + "your Safe Word was " + userWord);
-        p.append("<p> Great. Press Next to Proceed </p>");
-        $("#compare").append(p);
-       }
-       else {
-        var p = $("<p>");
-        p.append("You entered: " + verify + "your Safe Word was " + userWord);
-        p.append("<p> It's probably time to go home. </p>");
-        $("#compare").append(p);
-       }
-
-    })
 
 })
